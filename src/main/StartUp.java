d15310fa6e774545aaf5c7d2abc89a98ad93be1d
Ziggy Moens <ziggy.moens@student.hogent.be>
2020-02-13 18:01:11 +0100

    structure

--- src/main/StartUp.java
+++ src/main/StartUp.java
@@ -1,6 +1,6 @@
 package main;
 
-public class TesterClass {
+public class StartUp {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Hello world v2");
--- src/persistentie/Persisentie1.java
+++
@@ -1,4 +0,0 @@
-package persistentie;
-
-public class Persisentie1 {
-}
