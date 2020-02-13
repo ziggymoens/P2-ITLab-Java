a4494f1239b68ea737da99c506c5dcaaf06e22bc
Ziggy Moens <ziggy.moens@student.hogent.be>
2020-02-13 17:22:43 +0100

    Test

--- src/TesterClass.java
+++ src/TesterClass.java
@@ -3,5 +3,6 @@ public class TesterClass {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Hello world v2");
+		System.out.println("test");
 	}
 }
