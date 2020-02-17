43e691729da1b5cd74ae9be8eb5f6e3d63d91f0f
Jonathan Vanden Eynden <47554911+JonathanVandenEynden@users.noreply.github.com>
2020-02-17 19:10:40 +0100

    taalfout gebruiktersstatus

--- src/domein/Gebruikersstatus.java
+++ src/domein/Gebruikersstatus.java
@@ -4,5 +4,5 @@ package domein;
  * Deze enum bevat de gebruiksersstatussen die de gebruikers kunnen aannemen
  */
 public enum Gebruikersstatus {
-    ACTIEF, GEBLOKEERD, NIET_ACTIEF
+    ACTIEF, GEBLOKKEERD, NIET_ACTIEF
 }
