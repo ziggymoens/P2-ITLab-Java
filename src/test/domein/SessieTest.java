29e1633e5e19a417f0f1297e267c311d81c55984
Kilian Hoefman <kilian-hoefman@hotmail.com>
2020-02-18 11:23:56 +0100

    SessieTest v1 + Controle in Sessie (begin)

--- src/test/domein/SessieTest.java
+++ src/test/domein/SessieTest.java
@@ -1,7 +1,30 @@
 package test.domein;
 
+import domein.Gebruiker;
+import domein.Lokaal;
+import exceptions.domein.SessieException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.time.LocalDateTime;
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SessieTest {
+
+    private static Stream<Arguments> opsomminsOngeldigeWaarden(){
+        return Stream.of(Arguments.of("", ""));
+    }
+
+    @ParameterizedTest
+    @MethodSource("opsomminsOngeldigeWaarden")
+    public void maakSessieOngeldigeWaarden_GooitException(String titel, LocalDateTime startSessie, Lokaal lokaal, Gebruiker verantwoordelijke){
+        Assertions.assertThrows(SessieException.class, () -> {
+
+        });
+    }
 
 }
