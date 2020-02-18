d65a4fccce23dbdf7141739403193beb3d66fa8a
Kilian Hoefman <kilian-hoefman@hotmail.com>
2020-02-18 18:51:07 +0100

    Gebruiker volledig getest, geslaagd

--- src/test/domein/GebruikerTest.java
+++ src/test/domein/GebruikerTest.java
@@ -5,6 +5,7 @@ import domein.Gebruikersprofielen;
 import domein.Gebruikersstatus;
 import exceptions.domein.GebruikerException;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -25,11 +26,29 @@ public class GebruikerTest {
                 Arguments.of("Test Persoon", "123456tp", "NIETSNUT", ""));
     }
 
+    private static Stream<Arguments> opsommingGeldigeWaarden(){
+        return Stream.of(Arguments.of("Test Persoon", "123456tp", "GEBRUIKER", "ACTIEF"),
+                Arguments.of("Test Persoon", "123456tp", "VERANTWOORDELIJKE", "ACTIEF"),
+                Arguments.of("Test Persoon", "123456tp", "HOOFDVERANTWOORDELIJKE", "ACTIEF"),
+                Arguments.of("Test Persoon", "123456tp", "GEBRUIKER", "GEBLOKKEERD"),
+                Arguments.of("Test Persoon", "123456tp", "GEBRUIKER", "NIET_ACTIEF"));
+    }
+
     @ParameterizedTest
     @MethodSource("opsommingOngeldigeWaarden")
     public void maakGebruikerAanZonderProfielFotoFouteWaarden_GooitException(String naam, String gebruikersnaam, String type, String status){
         Assertions.assertThrows(GebruikerException.class, () -> {
-            Gebruiker geb = new Gebruiker(naam, gebruikersnaam, type, status);
+            new Gebruiker(naam, gebruikersnaam, type, status);
         });
+    }
+
+    @ParameterizedTest
+    @MethodSource("opsommingGeldigeWaarden")
+    public void maakGebruikerAanZonderProfielFotoCorrecteWaarden_Slaagt(String naam, String gebruikersnaam, String type, String status){
+        Gebruiker gebruiker = new Gebruiker(naam, gebruikersnaam, type, status);
+        Assertions.assertEquals(naam, gebruiker.getNaam());
+        Assertions.assertEquals(gebruikersnaam, gebruiker.getGebruikersnaam());
+        Assertions.assertEquals(type, gebruiker.getGebruikersprofielen().toString());
+        Assertions.assertEquals(status, gebruiker.getStatus().toString());
     }
 }
