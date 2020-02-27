1b0eca9fcbd9def4459db38863bd6cc46181bdea
Ziggy Moens <ziggy.moens@student.hogent.be>
2020-02-27 19:40:17 +0100

    jpa

--- src/domein/SessieKalenderDataInit.java
+++ src/domein/SessieKalenderDataInit.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SessieKalenderDataInit {
-    private final File lokalen = new File("csv/Lokalen");
+    private final File lokalen = new File("src/csv/Lokalen");
     private SessieKalender sessieKalender;
 
     public SessieKalenderDataInit(SessieKalender sessieKalender){
@@ -31,15 +31,6 @@ public class SessieKalenderDataInit {
         }
 
 
-
-        Lokaal lokaal1 = new Lokaal("GSCHB1.017", 50);
-        Lokaal lokaal2= new Lokaal("GSCHB3.019", 35);
-        Lokaal lokaal3 = new Lokaal("GSCHB4.036", 24);
-        Lokaal lokaal4 = new Lokaal("GSCHB1.012", 53);
-        Lokaal lokaal5 = new Lokaal("GSCHB0.010", 200);
-        Lokaal lokaal6 = new Lokaal("GSCHB4.026", 30);
-        Lokaal lokaal7 = new Lokaal("GSCHC0.125", 175);
-
         Gebruiker gebruiker1 = new Gebruiker("Ziggy Moens", "758095zm", Gebruikersprofielen.HOOFDVERANTWOORDELIJKE, Gebruikersstatus.ACTIEF);
         Gebruiker gebruiker2 = new Gebruiker("Kilian Hoefman", "757932kh", Gebruikersprofielen.GEBRUIKER, Gebruikersstatus.ACTIEF);
         Gebruiker gebruiker3 = new Gebruiker("Jonathan Vanden Eynden", "862361jv", Gebruikersprofielen.GEBRUIKER, Gebruikersstatus.ACTIEF);
@@ -50,11 +41,11 @@ public class SessieKalenderDataInit {
 
 
         Sessie sessie1 = new Sessie("Wat de recruteringsafdeling van jou verwacht", LocalDateTime.now().plusMinutes(1000), LocalDateTime.now().plusMinutes(1200), sessieKalender.geefLokaalById("GSCHB1.017"), gebruiker1);
-        Sessie sessie2 = new Sessie("Internationaal samenwerken", LocalDateTime.now().plusMinutes(2000), LocalDateTime.now().plusMinutes(2100), lokaal2, gebruiker2);
-        Sessie sessie3 = new Sessie("Inleiding tot Trello", LocalDateTime.now().plusMinutes(3500), LocalDateTime.now().plusMinutes(3550),lokaal3, gebruiker4);
-        Sessie sessie4 = new Sessie("Inleiding tot GIT", LocalDateTime.now().plusMinutes(4500), LocalDateTime.now().plusMinutes(4590), lokaal1, gebruiker1);
-        Sessie sessie5 = new Sessie("inleiding tot UNIX", LocalDateTime.now().plusMinutes(3000), LocalDateTime.now().plusMinutes(3120), lokaal7, gebruiker5);
-        Sessie sessie6 = new Sessie("IT in het leger", LocalDateTime.now().plusMinutes(1000), LocalDateTime.now().plusMinutes(1060), lokaal5, gebruiker1);
+        Sessie sessie2 = new Sessie("Internationaal samenwerken", LocalDateTime.now().plusMinutes(2000), LocalDateTime.now().plusMinutes(2100), sessieKalender.geefLokaalById("GSCHB3.019"), gebruiker2);
+        Sessie sessie3 = new Sessie("Inleiding tot Trello", LocalDateTime.now().plusMinutes(3500), LocalDateTime.now().plusMinutes(3550),sessieKalender.geefLokaalById("GSCHB4.036"), gebruiker4);
+        Sessie sessie4 = new Sessie("Inleiding tot GIT", LocalDateTime.now().plusMinutes(4500), LocalDateTime.now().plusMinutes(4590), sessieKalender.geefLokaalById("GSCHB1.017"), gebruiker1);
+        Sessie sessie5 = new Sessie("inleiding tot UNIX", LocalDateTime.now().plusMinutes(3000), LocalDateTime.now().plusMinutes(3120), sessieKalender.geefLokaalById("GSCHC0.125"), gebruiker5);
+        Sessie sessie6 = new Sessie("IT in het leger", LocalDateTime.now().plusMinutes(1000), LocalDateTime.now().plusMinutes(1060), sessieKalender.geefLokaalById("GSCHB0.010"), gebruiker1);
 
         sessieKalender.voegSessieToe(sessie1);
         sessieKalender.voegSessieToe(sessie2);
@@ -62,8 +53,6 @@ public class SessieKalenderDataInit {
         sessieKalender.voegSessieToe(sessie4);
         sessieKalender.voegSessieToe(sessie5);
         sessieKalender.voegSessieToe(sessie6);
-        sessieKalender.voegLokaalToe(lokaal4);
-        sessieKalender.voegLokaalToe(lokaal6);
 
     }
 }
