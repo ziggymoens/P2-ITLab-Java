9fca3a22dba562d33d06a4697e6915be1c50e68b
Kilian Hoefman <kilian.hoefman@student.hogent.be>
2020-03-02 20:56:39 +0100

    Geeft gebruiker weer adhv nieuwe methodes

--- src/userinterface/GebruikerBeheren/InfoGebruikerController.java
+++ src/userinterface/GebruikerBeheren/InfoGebruikerController.java
@@ -4,14 +4,13 @@ import domein.DomeinController;
 import domein.interfacesDomein.IGebruiker;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import userinterface.MAIN.MainScreenController;
 
 import java.io.IOException;
+import java.time.format.DateTimeFormatter;
 
 public class InfoGebruikerController extends AnchorPane {
 
@@ -30,6 +29,9 @@ public class InfoGebruikerController extends AnchorPane {
     private TableView<IGebruiker> tableViewGebrukers;
 
     @FXML
+    private TableColumn<IGebruiker, String> TVnaamGebruiker, TVgebruikersnaamChamilo, TVstatus, TVtype;
+
+    @FXML
     private Button zoekKnop, maakAanKnop, bewerkKnop, verwijderKnop;
 
     private DomeinController domeinController;
@@ -46,6 +48,16 @@ public class InfoGebruikerController extends AnchorPane {
             throw new RuntimeException();
         }
 
+        vulTable();
+    }
+
+    public void vulTable(){
+        tableViewGebrukers.getColumns().clear();
+        TVnaamGebruiker.setCellValueFactory(new PropertyValueFactory<>("naam"));
+        TVgebruikersnaamChamilo.setCellValueFactory(new PropertyValueFactory<>("gebruikersnaam"));
+        TVstatus.setCellValueFactory(new PropertyValueFactory<>("status"));
+        TVtype.setCellValueFactory(new PropertyValueFactory<>("gebruikersprofiel"));
         tableViewGebrukers.setItems(domeinController.geefIGebruikers());
+        tableViewGebrukers.getColumns().addAll(TVnaamGebruiker, TVgebruikersnaamChamilo, TVstatus, TVtype);
     }
 }
