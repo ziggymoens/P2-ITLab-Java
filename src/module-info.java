31a87f6bc3c318006f86f098ade159f34352b082
Ziggy Moens <ziggy.moens@student.hogent.be>
2020-03-24 11:31:37 +0100

    module-info

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+open module jpaEnJavaFx {
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.fxml;
+
+    requires java.instrument;
+    requires java.sql;
+}
